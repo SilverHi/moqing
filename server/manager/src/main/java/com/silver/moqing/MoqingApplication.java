db870d27d8ece90e5b2c63c97d156fb523d9a30f
SilverHi <molei8686138@gmail.com>
2021-02-19 19:58:56 +0800

    init

---
+++ server/manager/src/main/java/com/silver/moqing/MoqingApplication.java
@@ -0,0 +1,13 @@
+package com.silver.moqing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoqingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoqingApplication.class, args);
+    }
+
+}
