4044dc9c39fa473f7c3fab6dad7a8b61a952b993
SilverHi <molei8686138@gmail.com>
2021-03-01 21:42:31 +0800

    login

---
+++ front-end/moqin-vue/src/components/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    error
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'MainPage'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ front-end/moqin-vue/src/components/login/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <label>用户名：</label>
+      <input type="text" name="username" value=""/>
+    </div>
+    <div>
+      <label>密码：</label>
+      <input type="password" name="password" value=""/>
+    </div>
+    <div>
+      <el-button type="primary" round>登录</el-button>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
